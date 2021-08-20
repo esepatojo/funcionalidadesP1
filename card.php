1504239f856ed7b86a6a0806decba33c3f322177
esepatojo <68978152+esepatojo@users.noreply.github.com>
2021-08-20 01:17:45 -0600

    Nuevo archivo
    Agregue funcionalidad card

---
+++ card.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!--<title>Hello, world!</title>-->
+  </head>
+  <body>
+    <!--<h1>Hello, world!</h1>-->
+    
+    <h1 class="container mt-4">Funcionalidades Bootstrap My Game Time</h1>
+    
+<h4 class="container text-secondary">Tarjetas en Bootstrap</h4>
+<div class="row row-cols-1 row-cols-md-3">
+  <div class="col">
+    <div class="card">
+    <div class="card text-white bg-success mb-3">
+      <img class="card-img-top" src="https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2020/05/xbox-logo-1934939.jpg?itok=jgyqCT3m" alt="XBOX">
+      <div class="card-body">
+        <h4 class="card-title">XBOX</h4>
+        <hr>
+        <p class="card-text lh-sm">Rex is the Mayor of Binaryville and a well-loved personality in town. He rose to robotdom from a microprocessor plant on the south side of town, where many famous and influential robots before him were conceived.</p>
+        <a href="#" class="card-link btn btn-dark mt-auto">Leer</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="col">
+  <div class="card">
+  <div class="card text-white bg-primary mb-3">
+    <img class="card-img-top" src="https://static.pisapapeles.net/uploads/2021/02/PlayStation-logo.png" alt="PLAY STATION">
+    <div class="card-body">
+      <h4 class="card-title">PLAY STATION</h4>
+      <hr>
+      <p class="card-text lh-sm">Dolores robot is a computational genius who performs calculations for various Binaryville businesses and organizations. In her spare time, Dolores has been noodling on the creation of the perfect web browser that.</p>
+      <a href="#" class="card-link btn btn-dark mt-auto">Leer</a>
+    </div>
+  </div>
+ </div>
+</div>
+
+<div class="col">
+  <div class="card">
+  <div class="card text-white bg-danger mb-3">
+    <img class="card-img-top" src="https://www.nintenderos.com/wp-content/uploads/2021/05/nintendo-switch-pro-logo1.jpg" alt="NINTENDO SWITCH">
+    <div class="card-body">
+      <h4 class="card-title">NINTENDO SWITCH</h4>
+      <hr>
+      <p class="card-text lh-sm">Es una consola de videojuegos desarrollada por Nintendo,se puede utilizar como consola de sobremesa con la unidad principal insertada en una estación de acoplamiento para conectarla con un televisor.</p>
+      <a href="#" class="card-link btn btn-dark mt-auto">Leer</a>
+    </div>
+  </div>  
+</div>
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
